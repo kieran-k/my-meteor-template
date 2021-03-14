99720475bc514410869b08bf577acfa86ac43017
kk <58120049+kierankjk@users.noreply.github.com>
2021-03-13 20:48:24 -1000

    wod

--- app/imports/ui/components/StuffItem.jsx
+++ app/imports/ui/components/StuffItem.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Table } from 'semantic-ui-react';
+import { Table, Button, Icon } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter, Link } from 'react-router-dom';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class StuffItem extends React.Component {
+
+  removeItem(docID) {
+    console.log(`item to delete is: ${docID}`);
+    this.props.Stuffs.collection.remove(docID);
+  }
+
   render() {
     return (
       <Table.Row>
@@ -13,6 +19,11 @@ class StuffItem extends React.Component {
         <Table.Cell>{this.props.stuff.condition}</Table.Cell>
         <Table.Cell>
           <Link to={`/edit/${this.props.stuff._id}`}>Edit</Link>
+        </Table.Cell>
+        <Table.Cell>
+          <Button icon onClick={() => this.removeItem(this.props.stuff._id)}>
+            <Icon name='trash' />
+          </Button>
         </Table.Cell>
       </Table.Row>
     );
@@ -27,6 +38,7 @@ StuffItem.propTypes = {
     condition: PropTypes.string,
     _id: PropTypes.string,
   }).isRequired,
+  Stuffs: PropTypes.object.isRequired,
 };
 
 // Wrap this component in withRouter since we use the <Link> React Router element.
